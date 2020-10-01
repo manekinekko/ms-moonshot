431fdcca6bc55cc89b30fb7286070fa03fabe832
Wassim Chegham <github@wassim.dev>
2020-10-01 11:56:53 +0200

    feat: add live preview mode

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-"use strict";
+import { anonymize, getSetting } from "./utils.js";
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -6,11 +6,18 @@ chrome.runtime.onInstalled.addListener(function () {
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlMatches: "(azure|github|microsoft)\.com" },
+            pageUrl: { urlMatches: "(azure|github|microsoft).com" },
           }),
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()],
       },
     ]);
   });
+});
+
+chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+  if (changeInfo.status == "complete" && tab.active) {
+    const shouldAnonymize = await getSetting("anonymize");
+    await anonymize(shouldAnonymize);
+  }
 });
